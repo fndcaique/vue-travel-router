d7c1a0013fe059c6104fb74a0e81d09276723f12
Fernando Soares <fndcaique@gmail.com>
2021-02-19 13:22:31 -0300

    add DestinationDetails

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,9 +1,64 @@
 <template>
-  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/brazil">Brazil</router-link>
-      <router-link to="/jamaica">Jamaica</router-link>
-      <router-link to="/panama">Panama</router-link>
-      <router-link to="/hawaii">Hawaii</router-link>
-    </div>
+  <nav id="nav">
+    <p class="logo">The Vue School Travel App</p>
+    <ul class="nav-links">
+      <li class="link">
+        <router-link to="/">Home</router-link> |
+      </li>
+      <li
+        class="link"
+        v-for="destination in destinations"
+        :key="destination.name"
+      >
+        <router-link :to="{
+          name: 'DestinationDetails',
+          params: {
+            id: destination.id
+          }
+        }">{{destination.name}}</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
+<script>
+import store from '@/store';
+
+export default {
+  data() {
+    return {
+      destinations: store.destinations,
+    };
+  },
+};
+</script>
+<style scoped>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.nav-links {
+  display: flex;
+}
+
+.link {
+  padding-right: 20px;
+  list-style: none;
+}
+
+.logo {
+  font-size: 20px;
+  color: #42b983;
+  font-weight: bold;
+}
+</style>
---
+++ src/views/DestinationDetails.vue
@@ -0,0 +1,27 @@
+<template>
+<section class="destination">
+  <h1>{{destination.name}}</h1>
+  <div class="destination-details">
+    <img :src="require(`@/assets/${destination.image}`)" alt="">
+  </div>
+</section>
+</template>
+
+<script>
+import store from '@/store';
+
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+    };
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        (destination) => destination.id === this.destinationId,
+      );
+    },
+  },
+};
+</script>
